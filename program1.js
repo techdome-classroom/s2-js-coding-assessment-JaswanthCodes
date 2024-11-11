4040d8b9233f45942a09253895a9a5dad77db1cd
Jaswanth <palletijaswanth@gmail.com>
2024-11-11 15:30:27 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -12,17 +12,14 @@ var isValid = function(s) {
 
     for (let char of s) {
         if (char === '(' || char === '{' || char === '[') {
-            // Push opening brackets onto the stack
             stack.push(char);
         } else {
-            // For closing brackets, check if they match the top of the stack
             if (stack.length === 0 || stack.pop() !== map[char]) {
                 return false;
             }
         }
     }
 
-    // If stack is empty, all brackets were matched correctly
     return stack.length === 0;
 };
 
